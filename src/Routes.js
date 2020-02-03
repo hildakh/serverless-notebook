4fb12ee6d3200470191fd7286158c9158a32f4d4
Hilda <hilda.kh.amiri@gmail.com>
2020-02-03 15:51:36 -0800

    notfound added to routes to catch all the routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Home from './components/Home';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import NotFound from "./components/NotFound";
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={Home} />
+      {/* Finally, catch all unmatched routes */}
+      <Route component={NotFound} />
     </Switch>
   );
 }

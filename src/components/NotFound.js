d6ed63106d5365acb4ef135b219c9ffa8138fa54
Hilda <hilda.kh.amiri@gmail.com>
2020-02-03 15:52:15 -0800

    spell change

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -4,7 +4,7 @@ import './NotFound.css';
 export default function NotFound() {
   return (
     <div className="NotFound">
-      <h3>Sorry, Page non trouvée!</h3>
+      <h3>Sorry, page non trouvée!</h3>
     </div>
   );
 }

887f6b6d5d002efd14a2e944b5976155a36e8d34
Hilda <hilda.kh.amiri@gmail.com>
2020-02-03 13:16:01 -0800

    logo removed. index file changed to render using router

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
-import React from 'react';
-// import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Navbar } from "react-bootstrap";
+import "./App.css";
 
-function App() {
+export default function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <img src="https://cdn.pixabay.com/photo/2017/04/19/13/07/coffee-2242231_960_720.jpg" alt="notes" className="App-pic"></img>
-        <p>
-          Hilda's serverless note-taking app
-        </p>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-      </header>
+    <div className="App container">
+      <Navbar fluid collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to="/">Scratch</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+      </Navbar>
     </div>
   );
 }
 
-export default App;

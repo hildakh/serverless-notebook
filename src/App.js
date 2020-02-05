130fd3d331ca2d26fdacf2e842b039b3e906c598
Hilda <hilda.kh.amiri@gmail.com>
2020-02-04 16:48:12 -0800

    usestate added to app to keep track of users being logged in or not

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Nav, Navbar, NavItem } from "react-bootstrap";
 import "./App.css";
@@ -6,6 +6,8 @@ import Routes from "./Routes";
 import { LinkContainer } from "react-router-bootstrap";
 
 export default function App(props) {
+
+  const [isAuthenticated, setAuthenticated] = useState(false);
   return (
     <div className="App container">
       <Navbar fluid collapseOnSelect>

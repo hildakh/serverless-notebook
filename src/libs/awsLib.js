e041525a95c7f0e3236b8ea37b2a0a40fb1fab01
Hilda <hilda.kh.amiri@gmail.com>
2020-02-09 14:41:13 -0800

    function to store the attachment to S3 storage added

---
+++ src/libs/awsLib.js
@@ -0,0 +1,11 @@
+import { Storage } from 'aws-amplify';
+export async function s3Upload(file) {
+  const filename = `${Date.now()}-${file.name}`;
+
+  // Storage.put will store the file object publicly while Storage.vault.put will store it privately
+  const stored = await Storage.vault.put(filename, file, {
+    contentType: file.type
+  });
+
+  return stored.key
+}

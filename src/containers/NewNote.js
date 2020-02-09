8985d9f6705a5a6d1bffa04c8d66cb944b54cda6
Hilda <hilda.kh.amiri@gmail.com>
2020-02-09 13:02:46 -0800

    used api from aws-amplify to create a new note and send a post request to /notes

--- src/containers/NewNote.js
+++ src/containers/NewNote.js
@@ -1,8 +1,9 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, createContext } from 'react';
 import { FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
 import LoaderButton from '../components/LoaderButton';
 import config from '../config';
 import './NewNote.css';
+import { API } from 'aws-amplify';
 
 export default function NewNote(props) {
   // useRef hook does not cause the component to rerender
@@ -20,6 +21,13 @@ export default function NewNote(props) {
     file.current = event.target.files[0]
   }
 
+  function createNote(note) {
+    // "notes" is the name the I chose for these set of APIs when configuring AWS Amplify >> check it in src/index.js
+    return API.post("notes", "/notes", {
+      body: note
+    });
+  }
+
   async function handleSubmit(event) {
     event.preventDefault();
 
@@ -30,6 +38,15 @@ export default function NewNote(props) {
       return;
     }
     setIsLoading(true);
+
+    try {
+      await createNote({ content });
+      // redirects the user to the home page after creating a note
+      props.history.push("/");
+    } catch (e) {
+      alert(e);
+      setIsLoading(false);
+    }
   }
 
   return (

f7e0f6efb50ad06fff7349b60a2386f3da8330b3
Hilda <hilda.kh.amiri@gmail.com>
2020-02-09 13:03:16 -0800

    ceate context removed

--- src/containers/NewNote.js
+++ src/containers/NewNote.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, createContext } from 'react';
+import React, { useState, useRef } from 'react';
 import { FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
 import LoaderButton from '../components/LoaderButton';
 import config from '../config';

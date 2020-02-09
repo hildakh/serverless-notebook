10f63e59c2b1e3604c48951b25eaf84ca35944d0
Hilda <hilda.kh.amiri@gmail.com>
2020-02-09 09:38:47 -0800

    handle submit, form validation and handle file change added

--- src/containers/NewNote.js
+++ src/containers/NewNote.js
@@ -1,6 +1,28 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import { FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
 import LoaderButton from '../components/LoaderButton';
 import config from '../config';
 import '../NewNote.css';
 
+export default function NewNote(props) {
+  // useRef hook does not cause the component to rerender
+  //the browser will handle the state of the file rather than the component
+  const file = useRef(null);
+  const [content, setContent] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  function validateForm() {
+    return content.length > 0;
+  }
+
+  function handleFileChange(event) {
+    // We can set/get the current value of a ref by using its current property.
+    console.log(event.target)
+    file.current = event.target.files[0]
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+  }
+}

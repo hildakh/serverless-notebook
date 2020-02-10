fbad67216d1387ef01c3c3d096dee7effeba7380
Hilda <hilda.kh.amiri@gmail.com>
2020-02-10 10:35:55 -0800

    notes were stored in the state. notes are listed if the user has been authenticated. Lander is rendered if the user hasn't been authenticated

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Home.css";
+import { PageHeader, ListGroup } from "react-bootstrap";
+// import { getBsProps } from "react-bootstrap/lib/utils/bootstrapUtils";
 
-export default function Home() {
+export default function Home(props) {
+  const [notes, setNotes] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  function renderLander() {
+    return (
+      <div className="lander">
+        <p>A simple note taking app created by Hilda</p>
+      </div>
+    );
+  }
+
+  function renderNotesList(notes) {
+    return null;
+  }
+
+
+  function renderNotes() {
+    return (
+      <div className="notes">
+        <PageHeader>Your Notes</PageHeader>
+        <ListGroup>
+          { !isLoading && renderNotesList(notes)}
+        </ListGroup>
+      </div>
+    );
+  }
+
   return (
     <div className="Home">
       <div className="lander">
-        <h1>Scratch</h1>
-        <p>A simple note taking app created by Hilda</p>
+        { props.isAuthenticated ? renderNotes() : renderLander()}
       </div>
     </div>
   );

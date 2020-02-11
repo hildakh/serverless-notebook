a4cc3d1b99701212b2795f25100b14cbf75077bf
Hilda <hilda.kh.amiri@gmail.com>
2020-02-11 11:58:46 -0800

    fix failed

--- src/config.js
+++ src/config.js
@@ -1,18 +1,20 @@
+const env = require('dotenv');
+env.config();
 export default {
   MAX_ATTACHMENT_SIZE: 5000000,
-  // identityPoolRegion: 'us-east-2',
+
   s3: {
-    REGION: "us-east-2",
-    BUCKET: "notes-hilda"
+    REGION: env.REGION,
+    BUCKET: env.BUCKET
   },
   apiGateway: {
-    REGION: "us-east-2",
-    URL: " https://qb7hs36mda.execute-api.us-east-2.amazonaws.com/prod"
+    REGION: env.REGION,
+    URL: env.URL
   },
   cognito: {
-    REGION: "us-east-2",
-    USER_POOL_ID: "us-east-2_LlssBgBau",
-    APP_CLIENT_ID: "3nanr14bsg1g98vd7dgg5u65hq",
-    IDENTITY_POOL_ID: "us-east-2:ca8bcdf7-b021-4d11-b552-3c843b3f857c"
+    REGION: env.REGION,
+    USER_POOL_ID: env.USER_POOL_ID,
+    APP_CLIENT_ID: env.APP_CLIENT_ID,
+    IDENTITY_POOL_ID: env.IDENTITY_POOL_ID
   }
 };

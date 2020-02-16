874dfe457e119151a5c74777f52ba94fe18caf5f
Hilda <hilda.kh.amiri@gmail.com>
2020-02-16 12:05:08 -0800

    function to delete the old attachment added

--- src/containers/Notes.js
+++ src/containers/Notes.js
@@ -4,7 +4,7 @@ import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import LoaderButton from "../components/LoaderButton";
 import config from "../config";
 import "./Notes.css";
-import { s3Upload } from "../libs/awsLib";
+import { s3Upload, s3Delete } from "../libs/awsLib";
 
 export default function Notes(props) {
   const file = useRef(null);
@@ -77,8 +77,8 @@ export default function Notes(props) {
 
     try {
       if(file.current) {
+        await s3Delete(note.attachment);
         attachment = await s3Upload(file.current);
-        // removeAttachment();
       }
 
       await saveNote({

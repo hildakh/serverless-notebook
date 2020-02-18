decf6be093e14d07b6d05dc141b5fde76ef87738
Hilda <hilda.kh.amiri@gmail.com>
2020-02-17 17:25:07 -0800

    billing form created to take user's name and credit card details and create a stripe token

---
+++ src/components/BillingForm.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import { FormGroup, FormControl, ControlLabel } from 'react-bootstrap' ;
+// The credit card number form is provided by the Stripe React SDK through the CardElement component
+import { CardElement, injectStripe } from 'react-stripe-elements';
+import LoaderButton from './LoaderButton';
+import { useFormFields } from '../libs/hooksLib';
+import './BillingForm.css';
+
+function BillingForm(props) {
+  const [fields, handleFieldChange] = useFormFields({
+    name: "",
+    storage: ""
+  });
+  const [isProcessing, setIsProcessing] = useState(false);
+  const [isCardComplete, setIsCardComplete] = useState(false);
+
+  props.isLoading = isProcessing || props.isLoading;
+
+  function validateForm() {
+    return (
+      fields.name !== "" &&
+      fields.storage !== "" &&
+      isCardComplete
+    );
+  }
+
+  async function handleSubmitClick(event) {
+    event.preventDefault();
+
+    setIsProcessing(true);
+
+    const { token, error } = await props.stripe.createToken({ name: fields.name});
+
+    setIsProcessing(false);
+
+    onsubmit(fields.storage, { token, error });
+  }
+
+  return (
+    <form className="BillingForm" onSubmit={handleSubmitClick}>
+      <FormGroup bsSize="large" controlId="storage">
+        <ControlLabel>Storage</ControlLabel>
+        <FormControl
+        min="0"
+        type="number"
+        value={fields.storage}
+        onChange={handleFieldChange}
+        placeholder="Number of notes to store"
+        />
+      </FormGroup>
+      {/* horizontal break to separate content */}
+      <hr />
+      <FormGroup bsSize="large" controlId="name">
+        {/* &apos;s escaping 's */}
+        <ControlLabel>Cardholder&apos;s name</ControlLabel>
+        <FormControl
+        type="text"
+        value={fields.name}
+        onChange={handleFieldChange}
+        placeholder="Name on the card"
+        />
+      </FormGroup>
+      <ControlLabel>Credit Card Info</ControlLabel>
+      <CardElement
+      className="card-field"
+      onChange={e => setIsCardComplete(e.complete)}
+      style={{ base: { fontSize: "18px", fontFamily: '"Open Sans", sans-serif'}
+    }}
+      />
+      <LoaderButton
+      block
+      type="submit"
+      bsSize="large"
+      isLoading={props.isLoading}
+      disabled={!validateForm()}
+      >
+        Purchase
+      </LoaderButton>
+    </form>
+  );
+}
+// wrapping the component using a stripe module injectStipe HOC (higher order component) gives us access to props.stripe.createToken method
+export default injectStripe(BillingForm);

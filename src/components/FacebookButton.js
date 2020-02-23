83653c1d151cd94eaadd362c1c2c90118edb21fc
Hilda <hilda.kh.amiri@gmail.com>
2020-02-22 23:53:36 -0800

    fb login component

---
+++ src/components/FacebookButton.js
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from 'react';
+import { Auth } from 'aws-amplify';
+import LoaderButton from './LoaderButton';
+
+function waitForInit() {
+  return new Promise((res, rej) => {
+    const hasFbLoaded = () => {
+      if (window.FB) {
+        res();
+      } else {
+        setTimeout(hasFbLoaded, 300);
+      }
+    };
+    hasFbLoaded();
+  });
+}
+
+export default function FacebookButton(props) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect( () => {
+    waitForInit();
+  }, []);
+
+  setIsLoading(false);
+
+  function handleError(error){
+    alert(error);
+  };
+
+  async function handleResponse(data) {
+    const { email, accessToken: token, expiresIn } = data;
+    const expires_at = expiresIn * 1000 + new Date().getTime();
+    const user = { email };
+
+    setIsLoading(true);
+
+    try {
+      const response = await Auth.federatedSignIn(
+        'facebook',
+        { token, expires_at },
+        user
+      );
+      setIsLoading(false);
+      props.onLogin(response);
+    } catch(e) {
+      setIsLoading(false);
+      handleError(e);
+    }
+  }
+
+  function statusChangeCallback(response){
+    if (response.status === 'connected') {
+      handleResponse(response.authResponse);
+    } else {
+      handleError(response);
+    }
+  };
+
+  function checkLoginState(){
+    window.FB.getLoginStatus(statusChangeCallback);
+  };
+
+  function handleClick(){
+    window.FB.login(checkLoginState, {scope: 'public_profile, email'});
+  };
+
+  return(
+    <LoaderButton
+    block
+    bsSize='large'
+    bsStyle='primary'
+    className='FacebookButton'
+    text='Login with Facebook'
+    onClick={handleClick}
+    disabled={isLoading}
+    />
+  );
+}

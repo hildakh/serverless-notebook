2ec79f72d89a74c770c357de4e14773ef550b895
Hilda <hilda.kh.amiri@gmail.com>
2020-03-07 10:34:52 -0800

    config variables added as env variabled to netlify

--- src/config.js
+++ src/config.js
@@ -2,17 +2,17 @@ const dev = {
   STRIPE_KEY: process.env.REACT_APP_STRIPE_KEY,
   s3: {
     REGION: "us-east-2",
-    BUCKET: "notes-app2-api-dev-attachmentsbucket-1u4ej5rfwaq7g"
+    BUCKET: process.env.REACT_APP_DEV_BUCKET
   },
   apiGateway: {
     REGION: "us-east-2",
-    URL: "https://9zy3xjvy32.execute-api.us-east-2.amazonaws.com/dev"
+    URL: process.env.REACT_APP_DEV_URL
   },
   cognito: {
     REGION: "us-east-2",
-    USER_POOL_ID: "us-east-2_eB1cnd5gk",
-    APP_CLIENT_ID: "4s29ge9s9h1u5ntccrlfokovve",
-    IDENTITY_POOL_ID: "us-east-2:d736566c-0793-4c84-846a-8b9c1c4db2b4"
+    USER_POOL_ID: process.env.REACT_APP_DEV_USER_POOL_ID,
+    APP_CLIENT_ID: process.env.REACT_APP_DEV_APP_CLIENT_ID,
+    IDENTITY_POOL_ID: process.env.REACT_APP_DEV_IDENTITY_POOL_ID
   }
 };
 
@@ -20,17 +20,17 @@ const prod = {
   STRIPE_KEY: process.env.REACT_APP_STRIPE_KEY,
   s3: {
     REGION: "us-east-2",
-    BUCKET: "notes-app2-api-prod-attachmentsbucket-ajh9yzga4y8j"
+    BUCKET: process.env.REACT_APP_PROD_BUCKET
   },
   apiGateway: {
     REGION: "us-east-2",
-    URL: "https://ctqvcdbadh.execute-api.us-east-2.amazonaws.com/prod"
+    URL: process.env.REACT_APP_PROD_URL
   },
   cognito: {
     REGION: "us-east-2",
-    USER_POOL_ID: "us-east-2_zwqjaqwwp",
-    APP_CLIENT_ID: "7v30ek7b1mn3au2v5rlpa3tb5d",
-    IDENTITY_POOL_ID: "us-east-2:5d3f1cbe-c611-495a-9d0c-1419eccded5e"
+    USER_POOL_ID: process.env.REACT_APP_PROD_USER_POOL_ID,
+    APP_CLIENT_ID: process.env.REACT_APP_PROD_APP_CLIENT_ID,
+    IDENTITY_POOL_ID: process.env.REACT_APP_PROD_IDENTITY_POOL_ID
   }
 };
 

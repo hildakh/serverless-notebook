6c6c832cdb94ef626935cf1401295a7bf67c7507
Hilda <hilda.kh.amiri@gmail.com>
2020-03-08 12:07:24 -0700

    lander style changed in home. Styling added to lander

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -3,6 +3,8 @@ import "./Home.css";
 import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import { API } from "aws-amplify";
+import { Link } from "react-router-dom";
+
 export default function Home(props) {
   const [notes, setNotes] = useState([]);
   const [isLoading, setisLoading] = useState(true);
@@ -31,7 +33,16 @@ export default function Home(props) {
   function renderLander() {
     return (
       <div className="lander">
-        <p>A simple note taking app created by Hilda</p>
+        <h1>Scratch</h1>
+        <p>A simple note taking app</p>
+        <div>
+          <Link to="/login" className="btn btn-info btn-lg">
+            Login
+          </Link>
+          <Link to="/signup" className="btn btn-success btn-lg">
+            Signup
+          </Link>
+        </div>
       </div>
     );
   }
